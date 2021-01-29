315e198b1113512f52bb4b71aae43d786e3ba644
Angus Trau <me@angus.ws>
2021-01-29 13:44:58 +1100

    Initial commit

---
+++ home.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, libs, ... }:
+{
+  imports = [
+    "./desktop"
+  ];
+
+  home.stateVersion = "20.09";
+  programs.home-manager.enable = true;
+
+  modules = {
+    desktop = {
+      firefox.enable = true;
+    };
+  };
+}
